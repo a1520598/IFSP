f72e80d7948580eec3d43a4ce9d9a380c555298f
a1520598 <a1520598@L2-08.hto.ifsp.edu.br>
2017-08-01 21:38:16 -0300

    Mais Frases

--- POO/src/HelloWorld.java
+++ POO/src/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("How are you? ");
+		System.out.println("I'm great! ");
+		System.out.println("What about you? ");
 	}
 
 }
